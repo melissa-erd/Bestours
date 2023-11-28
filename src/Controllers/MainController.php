871645653fad4ce3056738b1989b7dc9f61a7893
melissa <1@mail.ru>
2023-11-28 15:05:33 +0700

    Bestours1

--- src/Controllers/MainController.php
+++ src/Controllers/MainController.php
@@ -11,36 +11,46 @@ class MainController
 {
     public function index(View $view)
     {
-        return $view->make('index');
+        $tour = ORM::forTable('tours')->find_many();
+        return $view->make('index', [
+            'tours' => $tour]);
     }
 
     public function detail_page(View $view, $id)
     {
-        $review = ORM::forTable('reviews')->find_many();
         $tour = ORM::forTable('tours')->find_one($id);
+
+        $photos = ORM::forTable('photos')
+            ->select('*')
+            ->select('photos.photo', 'img')
+            ->join('tours', ['photos.tours_id', '=', 'tours.id'])
+            ->where('tours.id', $id)
+            ->find_many();
+
         return $view->make('detail-page', [
-            'items'=>$review,
-            'tour'=>$tour
+            'tours' => $tour,
+            'photos' => $photos
         ]);
+
     }
 
-    public function tour(ServerRequest $request)
-    {
-        $name = $request->getParsedBody()['name'];
-        $note = $request->getParsedBody()['note'];
-        $price = $request->getParsedBody()['price'];
-        $photo = $request->getParsedBody()['photo'];
-        $program_note = $request->getParsedBody()['program_note'];
-
-        $tour = ORM::for_table('tours')
-            ->where('name', $name)
-            ->where('note', $note)
-            ->where('price', $price)
-            ->where('photo', $photo)
-            ->where('program_note', $program_note)
-            ->find_one();
-        if ($tour == true) {
-            return new RedirectResponse('/detail-page/{id}');
-        }
-    }
+//    public function tour(ServerRequest $request)
+//    {
+//        $name = $request->getParsedBody()['name'];
+//        $note = $request->getParsedBody()['note'];
+//        $price = $request->getParsedBody()['price'];
+//        $photo = $request->getParsedBody()['photo'];
+//        $program_note = $request->getParsedBody()['program_note'];
+//
+//        $tour = ORM::for_table('tours')
+//            ->where('name', $name)
+//            ->where('note', $note)
+//            ->where('price', $price)
+//            ->where('photo', $photo)
+//            ->where('program_note', $program_note)
+//            ->find_one();
+//        if ($tour == true) {
+//            return new RedirectResponse('/detail-page/{id}');
+//        }
+//    }
 }
